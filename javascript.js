9b1edc0091c6b968a46b1e6bda25d53fa2009b7c
ivanwijayana <ivanwijayana@gmail.com>
2022-10-21 08:03:39 +0700

    update style

--- javascript.js
+++ javascript.js
@@ -69,72 +69,79 @@ $('document').ready(function(){
     
             if (options == "1.Menurun"){
                 if(answer1=="TAKJIL"){
+                    alert('Hebat, Jawabanya BENAR !!!')
                     for (var b = 9; b <= 69; b += 12){
                         $(`.jwb-${b}`).text(array1.shift());
                     }
                 }else{
-                    alert('jawaban salah')
+                    alert('Aduh Jawabanya Salah, Coba lagi yaaa !')
                 }          
             }
             // 
     
             if(options =="2.Menurun"){
                 if(answer1=="TADARUS"){
+                    alert('Hebat, Jawabanya BENAR !!!')
                     for (var b=18; b<=90; b+=12){
                         $(`.jwb-${b}`).text(array1.shift());
                     }               
                 }else{
-                    alert('jawaban salah')
+                    alert('Aduh Jawabanya Salah, Coba lagi yaaa !')
                 }  
             }
     
             if(options =="3.Menurun"){
                 if(answer1=="EIDALFITR"){
+                    alert('Hebat, Jawabanya BENAR !!!')
                     for (var b=23; b<=119; b+=12){
                         $(`.jwb-${b}`).text(array1.shift());
                     }               
                 }else{
-                    alert('jawaban salah')
+                    alert('Aduh Jawabanya Salah, Coba lagi yaaa !')
                 }  
             }
     
             if(options=="2.Mendatar"){
                 if(answer1=="TARAWEH"){
+                    alert('Hebat, Jawabanya BENAR !!!')
                     for (var b=18; b<=24; b++){
                         $(`.jwb-${b}`).text(array1.shift());
                     }               
                 }else{
-                    alert('jawaban salah')
+                    alert('Aduh Jawabanya Salah, Coba lagi yaaa !')
                 }  
             }
     
             if(options=="4.Mendatar"){
                 if(answer1=="RAMADAN"){
+                    alert('Hebat, Jawabanya BENAR !!!')
                     for (var b=49; b<=55; b++){
                         $(`.jwb-${b}`).text(array1.shift());
                     }               
                 }else{
-                    alert('jawaban salah')
+                    alert('Aduh Jawabanya Salah, Coba lagi yaaa !')
                 }  
             }
     
             if(options =="5.Mendatar"){
                 if(answer1=="HAUS"){
                     for (var b=76; b<=79; b++){
+                        alert('Hebat, Jawabanya BENAR !!!')
                         $(`.jwb-${b}`).text(array1.shift());
                     }               
                 }else{
-                    alert('jawaban salah')
+                    alert('Aduh Jawabanya Salah, Coba lagi yaaa !')
                 }  
             }
     
             if(options=="6.Mendatar"){
                 if(answer1=="SAHUR"){
+                    alert('Hebat, Jawabanya BENAR !!!')
                     for (var b=115; b<=119; b++){
                         $(`.jwb-${b}`).text(array1.shift());
                     }               
                 }else{
-                    alert('jawaban salah')
+                    alert('Aduh Jawabanya Salah, Coba lagi yaaa !')
                 }  
             }
         })
